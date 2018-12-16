7b3b939130a6028526f68bd5dd3b6716942b3c27
金喆 <jinzheisme@163.com>
2018-12-16 21:25:54 +0800

    some changes

--- gittest01/src/com/itheima/Test01.java
+++ gittest01/src/com/itheima/Test01.java
@@ -8,5 +8,8 @@ public class Test01 {
         System.out.println("aaaaaaaa");
         System.out.println("aaaaaaaa");
         System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
     }
 }
